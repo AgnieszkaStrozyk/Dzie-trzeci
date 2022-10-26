f8c78a3f153542bd55106dd9263681e6967e626c
Szkolenie <agnieszka.strozyk@gmail.com>
2022-10-26 19:31:15 +0200

    KROLIK

---
+++ Car.java
@@ -0,0 +1,6 @@
+public class Car {
+    int wheels = 4;
+    int seats = 4;
+    int lights = 6;
+    String colour = "red";
+}
---
+++ Rabbit.java
@@ -0,0 +1,12 @@
+public class Rabbit {
+    int length;
+    String name;
+    Boolean softness;
+
+    rabbit(int lenght; String name; boolean softness){
+        this.length =length;
+        this.name =name;
+        this.softness =softness;
+
+    }
+}
